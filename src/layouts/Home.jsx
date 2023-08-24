7de80c7824a92cc4d2bce16ccbbc7a5fd96f412f
AmalkeT <amxmulu123@gmail.com>
2023-08-24 12:02:31 +0300

    add styles

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -1,27 +1,47 @@
-import React from 'react'
-import SentencesWithDetails from './SentencesWithDetails'
-function Home() {
- const sentences = [
-    'this is the first sentence',
-    'this is the sencond sentence', 
-    'thisis the third sentence'
- ]
- const details = [
-    'this is the details of the first sentence',
-    'this is the details of the sencond sentence',
-    'this is the details of the third sentence'
- ]
+import React, { useState } from 'react'
+import { QA } from './SentencesWithDetails'
 
-  return (
-    <div>
-        {sentences.map((sentence,index) => (    
-        <SentencesWithDetails 
-         key = {index}
-        sentence = {sentence}
-        details = {details[index]}  />
-        ))}
-    </div>
-  )
+function Home() {
+   const [index, setIndex] = useState(0)
+   // const [isShowQ, setIsShowQ] = useState(false)
+   const [isShowA, setIsShowA] = useState(false)
+   let tests = QA[index]
+   const nextQA = index < QA.length - 1
+
+    const ClickNextButton = () => {
+      if(nextQA){
+         setIndex(index + 1)
+       } else {
+         setIndex(0)
+      }
+    }
+   // const showQuestion = () => {
+   //    setIsShowQ(!isShowQ)
+   // }
+
+   const showAnswer = () => {
+      setIsShowA(!isShowA)
+   }
+return (
+   <div className='flex justify-center w-1/2 h-72 mt-20 m-auto rounded-xl bg-lime-200'>
+         <div className=''>
+            <div className='text-end py-5'>
+               <button className=' bg-indigo-700 px-7 py-2 rounded-lg text-white text-lg ' onClick={ClickNextButton}>Next</button>
+            </div>
+            <div className='flex'>
+             <p className=' text-gray-500 border-indigo-700 px-4 py-2 border-2 rounded text-lg '>
+              Q: {tests.question} 
+             </p>
+             <span className='  ml-5 bg-indigo-700 items-center px-4 py-2 rounded-lg text-white cursor-pointer' onClick={showAnswer}> { isShowA ? 'Hide' : 'Show' }</span>
+            </div>
+         
+             <div className='py-2 px-5 fixed text-red-500'>   {isShowA && <p> Ans: {tests.answer}</p> }</div>     
+
+         </div>
+      
+   </div>
+ )
+    
 }
 
 export default Home
--- src/layouts/SentencesWithDetails.jsx
+++ src/layouts/SentencesWithDetails.jsx
@@ -1,22 +1,58 @@
-import React, { useState } from 'react'
-
-function SentencesWithDetails({details , sentence}) {
-    const [showDetails, setShowDetails] = useState(false)
-
-    const handelDetails = () => {
-        setShowDetails(!showDetails)
-    }
-  return (
-     <div className='p-10 border text-center'>
-     <p>{sentence}  <span onClick={handelDetails}>{showDetails ? "-" : "+" }</span></p>
-     {showDetails && (
-       <div>
-         {details}
-       </div>
-     ) }
-
-     </div>
-  )
-}
-
-export default SentencesWithDetails
+export const  QA = [
+  {
+    question: "Who was the first woman to fly solo across the Atlantic Ocean?",
+    answer:"Amelia Earhart"
+  },
+  {
+    question: "If you tipped 20% on a $15 bill, how much would the tip be?",
+    answer:"$3.00"
+  },
+  {
+    question: "What biological order do frogs belong to?",
+    answer:"Amphibians"
+  },
+  {
+    question: "What month was Julius Caesar stabbed in?",
+    answer:"March"
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    answer:"Leonardo da Vinci"
+  },
+  {
+    question: "Which country drinks the most beer per capita?",
+    answer:"Czech Republic"
+  },
+  {
+    question: "What is your body's largest organ?",
+    answer:"Skin"
+  },
+  {
+    question: "How many bones do sharks have?",
+    answer:"None"
+  },
+  {
+    question: " Who was the first woman to win a Nobel Prize?",
+    answer:"Marie Curie"
+  },
+  {
+    question: "Where is Gautemala located?",
+    answer:"Central America"
+  },
+  {
+    question: "Which two rivers did the Mesopotamian civilization rely on?",
+    answer:"The Tigris and Euphrates Rivers"
+  },
+  {
+    question: "What is the hottest planet in our solar system?",
+    answer:"Venus"
+  },
+  {
+    question: "Which city hosted the 2000 Summer Olympics?",
+    answer:"Sydney, Australia"
+  },
+  {
+    question: " What is the first astrological sign in the zodiac?",
+    answer:"Aries"
+  },
+]
